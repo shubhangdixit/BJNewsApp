f89f2efb041780fbf3045bddbc2afedbefa23ac7
Shubhang <subhangdxt@gmail.com>
2019-08-28 17:35:36 +0530

    added network Layer

---
+++ BJNewsApp/BJNewsApp/NetworkManager.swift
@@ -0,0 +1,67 @@
+//
+//  NetworkManager.swift
+//  BJNewsApp
+//
+//  Created by Shubhang Dixit on 28/08/19.
+//  Copyright © 2019 Shubhang. All rights reserved.
+//
+
+import Foundation
+
+public enum BJNewsAPIEndPoint : String {
+    case headLines = "https://newsapi.org/v2/top-headlines"
+    case everything = "https://newsapi.org/v2/everything"
+}
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    typealias successBlock  = (Data?) -> Void
+    typealias failureBlock = (Error?)-> Void
+    
+    func getData(forNewsType type : BJNewsAPIEndPoint, payload : [String : String], success: @escaping successBlock, failure: @escaping failureBlock) {
+        
+        var finalPayload = payload
+        finalPayload["apiKey"] = getAPIKey()
+        
+         
+    }
+    
+    func performRequest(forUrl url : String, requestType : String, success: @escaping successBlock, failure: @escaping failureBlock) {
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: URL(string: url)!)
+        request.httpMethod = requestType
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+            if let apiError = error {
+                failure(apiError)
+            } else {
+                if let result = data {
+                    success(result)
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    fileprivate func getAPIKey() -> String {
+        var nsDictionary: NSDictionary?
+        if let path = Bundle.main.path(forResource: "NewsApi", ofType: "plist") {
+            nsDictionary = NSDictionary(contentsOfFile: path)
+        }
+        return nsDictionary?.object(forKey: "APIKey") as! String
+    }
+    
+    func getURL(forEndPoint type: BJNewsAPIEndPoint, payload : [String : String]) -> String {
+        var urlString = type.rawValue
+        if payload.count > 0 {
+            urlString += "?"
+            for key in payload.keys {
+                let querry : String = key + "=" + (payload[key] ?? "")
+                urlString += querry
+            }
+        }
+        return urlString
+    }
+}
